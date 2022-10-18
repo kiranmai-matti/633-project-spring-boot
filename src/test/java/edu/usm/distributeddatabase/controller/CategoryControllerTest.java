00301067456d4dccc477c5116ea32c7a967eaceb
Kiranmai Matti <mattikiranmai007@gmail.com>
2022-10-17 23:47:52 -0500

    Added Unit testing

--- src/test/java/edu/usm/distributeddatabase/controller/CategoryControllerTest.java
+++ src/test/java/edu/usm/distributeddatabase/controller/CategoryControllerTest.java
@@ -1,19 +1,54 @@
 package edu.usm.distributeddatabase.controller;
 
 
-import edu.usm.distributeddatabase.repo.CategoryRepo;
+import edu.usm.distributeddatabase.entity.Category;
+import edu.usm.distributeddatabase.entity.Product;
+import edu.usm.distributeddatabase.mapper.ModelMapper;
+import edu.usm.distributeddatabase.model.CategoryDTO;
 import edu.usm.distributeddatabase.service.CategoryService;
+import edu.usm.distributeddatabase.service.ProductService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-//@RunWith(JUnitPlatform.class)
-public class CategoryControllerTest {
+class CategoryControllerTest {
     @InjectMocks
     CategoryController categoryController;
     @Mock
     CategoryService categoryService;
+    @Mock
+    ProductService productService;
+    @Mock
+    ModelMapper modelMapper;
+    @Mock
+    List<Category> categoryList;
+    @Mock
+    List<Product> productList;
+
+    @Test
+    void testGetCategories() {
+        when(categoryService.getAllCategories()).thenReturn(categoryList);
+        Assertions.assertDoesNotThrow(() -> categoryController.getCategories());
+        Assertions.assertInstanceOf(ResponseEntity.class, categoryController.getCategories());
+        Assertions.assertNotNull(categoryController.getCategories());
+    }
+
+    @Test
+    void testGetProductsByCategory() {
+        when(productService.getAllProductsByCategory(anyInt())).thenReturn(productList);
+        Assertions.assertDoesNotThrow(() -> categoryController.getProductsByCategory(1));
+        Assertions.assertInstanceOf(ResponseEntity.class, categoryController.getProductsByCategory(1));
+        Assertions.assertNotNull(categoryController.getProductsByCategory(1));
+    }
 
 }
--- src/test/java/edu/usm/distributeddatabase/controller/CustomerControllerTest.java
+++ src/test/java/edu/usm/distributeddatabase/controller/CustomerControllerTest.java
@@ -1,4 +1,46 @@
 package edu.usm.distributeddatabase.controller;
 
-public class CustomerControllerTest {
+import edu.usm.distributeddatabase.entity.Customer;
+import edu.usm.distributeddatabase.mapper.ModelMapper;
+import edu.usm.distributeddatabase.model.LoginRequest;
+import edu.usm.distributeddatabase.service.CustomerService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CustomerControllerTest {
+    @InjectMocks
+    CustomerController customerController;
+    @Mock
+    CustomerService customerService;
+    @Mock
+    Customer customer;
+    @Mock
+    ModelMapper modelMapper;
+
+    @Test
+    void testLoginCustomer() {
+        LoginRequest loginRequest = LoginRequest.builder().email("test@gmail.com").password("password").build();
+        when(customerService.getCustomerPresentByEmail(anyString())).thenReturn(customer);
+        when(customerService.validateCustomer(anyString(), anyString())).thenReturn(true);
+        Assertions.assertDoesNotThrow(() -> customerController.loginCustomer(loginRequest));
+        when(customerService.validateCustomer(anyString(), anyString())).thenReturn(false);
+        Assertions.assertDoesNotThrow(() -> customerController.loginCustomer(loginRequest));
+    }
+
+    @Test
+    void testRegisterCustomer() {
+        when(customerService.saveCustomer(any())).thenReturn(1);
+        Assertions.assertDoesNotThrow(() -> customerController.registerCustomer(customer));
+        Assertions.assertDoesNotThrow(() -> customerController.registerCustomer(customer));
+    }
+
 }
